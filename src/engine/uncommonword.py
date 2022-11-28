124e6d89459d620fd03bb3ad4db9a29babd9b0c9
UniqueName12345 <88826851+UniqueName12345@users.noreply.github.com>
2022-11-28 16:57:37 +0800

    change algorithm from Wikiparse to Hist

--- src/engine/uncommonword.py
+++ src/engine/uncommonword.py
@@ -1,14 +1,14 @@
 import requests
 import wikipedia
 
-url = "https://raw.githubusercontent.com/jeremy-rifkin/Wordlist/master/master.txt"
-
-response = requests.get(url)
-
-words = response.text.split("\n")
-
-words = [word for word in words if len(word) > 3]
-
-words = words[:1000]
-
-words.sort(key=lambda word: wikipedia.search(word)[0].lower() == word.lower())
+def initialize():
+    """
+    The initialize function downloads a list of words from the internet, sorts them by how many hits they get, then sorts them by the rarest few
+    :return: A list of 1000 words
+    """
+    url = "https://raw.githubusercontent.com/jeremy-rifkin/Wordlist/master/master.txt"
+    response = requests.get(url)
+    words = response.text.split("\n")
+    words = [word for word in words if len(word) > 3]
+    words = words[:1000]
+    words.sort(key=lambda word: len(requests.get(f"https://www.google.com/search?q={word}").text))
